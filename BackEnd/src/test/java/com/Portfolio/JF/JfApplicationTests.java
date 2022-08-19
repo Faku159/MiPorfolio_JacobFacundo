4cd3ee33286f8ac3ea639634206d654f1a882398
Faku159 <100450838+Faku159@users.noreply.github.com>
2022-08-18 21:04:00 -0300

    Codificacion en NetBeans

---
+++ BackEnd/src/test/java/com/Portfolio/JF/JfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Portfolio.JF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
